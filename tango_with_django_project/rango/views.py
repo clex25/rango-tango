df7d4082f09365dbdfdc476806d6044fe1406220
David Cleckley <cleckley@gmail.com>
2015-12-03 21:24:44 -0800

    templatized about page. added ice.jpg. updated about view

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -10,4 +10,8 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
     
 def about(request):
-    return HttpResponse("Rango says here is the about page <br/> <a href='/rango/'>Index</a>")
+    #construct dictionary to pass to template
+    context_dict = {'boldmessage': "cool"}
+    
+    #return a rendered response to send to client, using our index template
+    return render(request, 'rango/about.html', context_dict)
