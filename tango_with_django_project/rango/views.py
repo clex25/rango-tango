6c555e42bfbde921a518430734943b0c4f33e6c6
David Cleckley <cleckley@gmail.com>
2015-12-03 20:34:32 -0800

    changed index view to use index template

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hello world! <br/> <a href='/rango/about'>About</a>")
-
+    #construct dictionary to pass to template
+    context_dict = {'boldmessage': "I am bold from the context"}
+    
+    #return a rendered response to send to client, using our index template
+    return render(request, 'rango/index.html', context_dict)
+    
 def about(request):
     return HttpResponse("Rango says here is the about page <br/> <a href='/rango/'>Index</a>")
