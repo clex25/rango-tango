4032b840229200cb5b0c00c3abc1baa5362a1fe8
David Cleckley <cleckley@gmail.com>
2015-12-06 20:11:59 -0800

    added new category template and updated view. added top 5 categories and links to index. updated populate script with page views. updated URL handler to pass slugs

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from rango.models import Category, Page
+
 
 def index(request):
+    #Query DB for all categories. Show top 5 by likes in desc order
     #construct dictionary to pass to template
-    context_dict = {'boldmessage': "I am bold from the context"}
+    context_dict = {}
+    category_list = Category.objects.order_by('-likes')[:5]
     
+    context_dict['categories'] = category_list
+    
+    #get top 5 pages
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict['pages'] = page_list
+         
     #return a rendered response to send to client, using our index template
     return render(request, 'rango/index.html', context_dict)
     
@@ -15,3 +25,25 @@ def about(request):
     
     #return a rendered response to send to client, using our index template
     return render(request, 'rango/about.html', context_dict)
+
+def category(request, category_name_slug):
+    #rendering engine context dict
+    context_dict = {}
+    
+    try:
+        #try to find a category name slug
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+        
+        #get all the pages for that category
+        pages = Page.objects.filter(category=category)
+        
+        context_dict['pages'] = pages
+        
+        context_dict['category'] = category
+        
+    except Category.DoesNotExist:
+        #no category Found, display default Message
+        pass
+    
+    return render(request, 'rango/category.html', context_dict)
