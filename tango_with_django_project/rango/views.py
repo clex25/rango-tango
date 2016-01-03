5f010a6439d25ecf90a05b597998f0ef3c78e99c
David Cleckley <cleckley@gmail.com>
2016-01-03 14:53:17 -0800

    Chapter 10-implementing base template and inheriting in existing templates

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -151,7 +151,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Secret text for logged in users only")
+    return render(request, 'rango/restricted.html', {})
 
 @login_required
 def user_logout(request):
